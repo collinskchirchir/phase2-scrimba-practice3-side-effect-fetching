ed134906d884c505b55e8266dc7236f64e2f5969
Collins Chirchir <collinskchirchir@gmail.com>
2023-01-15 12:18:52 +0300

    fetch Todos on App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import NewTodo from './components/NewTodo';
 import TodoList from "./components/TodoList"
 
 function App() {
   const [todos, setTodos] = useState([]);
+  useEffect(
+    () => {
+      fetch("http://localhost:3000/todos")
+      .then(resp => resp.json())
+      .then(data => setTodos(data))
+    }
+    ,[]
+  );
 
   function addTodo(newTodo) {
     const updatedTodos = [...todos, newTodo]
